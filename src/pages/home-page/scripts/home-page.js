6063ef1e4b2a64075ea88af8fbf45954fed5ae5f
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-06 17:25:19 +0200

    Toggle input styles on focus

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,1 +1,21 @@
 import '../styles/home-page.scss'
+
+document.querySelectorAll('.input-container').forEach(input => {
+    const inputField = input.querySelector('input');
+    const labelWrapper = input.querySelector('.label-wrapper'); 
+
+    input.addEventListener('click', (e) => {
+        inputField.focus();
+    });
+
+    inputField.addEventListener('focus', () => {
+        labelWrapper.classList.add('label-wrapper--active');
+    });
+
+    inputField.addEventListener('blur', (e) => {
+        if(!inputField.value)
+            labelWrapper.classList.remove('label-wrapper--active');
+    });
+
+});
+
